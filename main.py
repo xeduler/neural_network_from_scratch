6d7add52ff61b38d7d5908340330de10620bf731
xeduler <mifaresamis@gmail.com>
2021-03-23 11:05:04 +0100

    some improvements

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import time
 import random
 import copy
 
+import tests
+
 #window settings
 WIDTH = 1000
 HIGHT = 1000
@@ -34,6 +36,13 @@ def calcSettings():
 
 calcSettings()
 
+canvas = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+]
 network = []
 
 pygame.init()
@@ -141,12 +150,49 @@ def disp(all = False):
     for l in range(DEEP_LAYERS+2):
         for n in range(len(network[l])):
             pygame.draw.circle(surface, (0, 255, 0) if network[l][n].output==network[l][n].output_level else (255, 0, 0), neuronPos(l, n), NEURON_RADIUS)
+    
+    #show canvas
+    for y in range(len(canvas)):
+        for x in range(len(canvas[y])):
+            if canvas[y][x]:
+                pygame.draw.rect(surface, (255, 255, 255), (x * 10, y*10, 10, 10))
+            else:
+                pygame.draw.rect(surface, (0, 0, 0), (x * 10, y*10, 10, 10))
+
     pygame.display.update()
+
+
+
+def updateInput():
+    for y in range(len(canvas)):
+        for x in range(len(canvas[y])):
+            network[0][y*len(canvas[y])+x].output = canvas[y][x]
+
+
+
+
+def updateNetwork():
+    for l in range(1, len(network)):
+        prev_output = []
+        for n in range(len(network[l-1])):
+            prev_output.append(network[l-1][n].output)
+        for n in range(len(network[l])):
+            network[l][n].calculate(prev_output)
+
+
+
+
+def randomCanvas():
+    for y in range(len(canvas)):
+        for x in range(len(canvas[y])):
+            canvas[y][x] = random.randint(0, 1)
 
 
 
 
 buildNetwork()
+updateInput()
+updateNetwork()
 disp(True)
 while(True):
     for e in pygame.event.get():
@@ -160,17 +206,54 @@ while(True):
             disp(True)
         if e.type == pygame.KEYDOWN:
             if e.key == pygame.K_r:
-                for n in range(len(network[0])):
-                    network[0][n].output = random.getrandbits(1)
-                for l in range(1, len(network)):
-                    prev_output = []
-                    for n in range(len(network[l-1])):
-                        prev_output.append(network[l-1][n].output)
-                    for n in range(len(network[l])):
-                        network[l][n].calculate(prev_output)
+                randomCanvas()
+                updateInput()
+                updateNetwork()
             if e.key == pygame.K_n:
                 network = []
                 buildNetwork()
+                updateInput()
+                updateNetwork()
                 disp(True)
+            if e.key == pygame.K_0:
+                canvas = copy.deepcopy(tests.shapes[0])
+                updateInput()
+                updateNetwork()
+            if e.key == pygame.K_1:
+                canvas = copy.deepcopy(tests.shapes[1])
+                updateInput()
+                updateNetwork()
+            if e.key == pygame.K_2:
+                canvas = copy.deepcopy(tests.shapes[2])
+                updateInput()
+                updateNetwork()
+            if e.key == pygame.K_3:
+                canvas = copy.deepcopy(tests.shapes[3])
+                updateInput()
+                updateNetwork()
+            if e.key == pygame.K_4:
+                canvas = copy.deepcopy(tests.shapes[4])
+                updateInput()
+                updateNetwork()
+            if e.key == pygame.K_5:
+                canvas = copy.deepcopy(tests.shapes[5])
+                updateInput()
+                updateNetwork()
+            if e.key == pygame.K_6:
+                canvas = copy.deepcopy(tests.shapes[6])
+                updateInput()
+                updateNetwork()
+            if e.key == pygame.K_7:
+                canvas = copy.deepcopy(tests.shapes[7])
+                updateInput()
+                updateNetwork()
+            if e.key == pygame.K_8:
+                canvas = copy.deepcopy(tests.shapes[8])
+                updateInput()
+                updateNetwork()
+            if e.key == pygame.K_9:
+                canvas = copy.deepcopy(tests.shapes[9])
+                updateInput()
+                updateNetwork()
     disp()
     clock.tick(FPS)
